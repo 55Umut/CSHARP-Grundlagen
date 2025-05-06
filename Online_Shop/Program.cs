8b0ce4b5231d12be861c51ba28e320a2111e47b6
55umut <123u@googlemail.com>
2025-05-06 10:27:13 +0200

    datenbank onlineshop

---
+++ Online_Shop/Program.cs
@@ -0,0 +1,18 @@
+﻿using Online_Shop.Properties.Models;
+using Online_Shop.Properties.Views;
+
+namespace Online_Shop
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DB_Produkte.Connect();
+            //DB_Produkte.ReadAll();
+            
+            Anzeige.MainMenu();
+            
+        }
+    }
+    
+}
